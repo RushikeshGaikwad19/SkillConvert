90b20b081674a56de7c488bc36b613c2ece06912
NA <rushikeshgaikwad2565@gmail.com>
2025-03-09 08:25:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Boot from "./components/boot";
+
+
+function App() {
+  return (
+    <div className="font-sans">
+      
+      <Boot/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/boot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Boot() { // Changed to 'Boot' instead of 'App'
+  return (
+    <div className="container">
+      <h1 className="text-center">Hello, Bootstrap!</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default Boot;
